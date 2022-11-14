2c1a763d24ce75804c2705f7d048d94dd3d408b3
Marina Pamparas <marpamparas@gmail.com>
2022-11-14 20:13:37 -0300

    agregué un array de objetos literales y una función superior

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+const vehiculosArray = [ 
+    { id: 1, nombre: "MOTO", precioHora: 80 },
+    { id: 2, nombre: "AUTO", precioHora: 100 },
+    { id: 3, nombre: "CAMIONETA", precioHora: 120 },
+]
+
+let iniciarReserva = document.getElementById ("botonIniciar")
+iniciarReserva.onclick = (parking)
+
+
 function parking () {
     let repeat = true
     while (repeat){
@@ -9,13 +19,13 @@ function parking () {
         console.log( confirmacion )
         
         if (confirmacion === true){
-            let horas = prompt ("Ingrese cantidad de horas:")
-            console.log ("Cantidad de horas: " + horas)
+            let horaUsuario = prompt ("Ingrese cantidad de horas:")
+            console.log ("Cantidad de horas: " + horaUsuario)
 
-            let vehiculo = prompt ("Ingrese tipo de vehiculo (moto, auto o camioneta): ")
-            console.log ("Vehiculo indicado: " + vehiculo)
+            let vehiculoUsuario = prompt ("Ingrese tipo de vehiculo (moto, auto o camioneta): ").toUpperCase()
+            console.log ("Vehiculo indicado: " + vehiculoUsuario)
             
-            let precioFinal = calcularMonto (horas, vehiculo)
+            let precioFinal = calcularMonto (horaUsuario, vehiculoUsuario, vehiculosArray)
             console.log ("Precio a abonar: " + precioFinal)
 
             if (precioFinal > 0){
@@ -35,18 +45,16 @@ function parking () {
 
     }
 }
-function calcularMonto (horas, vehiculo) {
-    switch (vehiculo){
-        case "moto": 
-            return horas * 80
-        case "auto":
-            return horas * 100
-        case "camioneta":
-            return horas * 120
-        default:
-            return 0
-    } 
-
+function calcularMonto (horas, vehiculo, array) {
+    let monto = ""
+        if (array.length > 0) {
+            array.forEach(elemento => {
+               if (vehiculo === elemento.nombre) {
+                monto = elemento.precioHora * horas
+               }
+            })
+            return monto
+        }
 }
 
 
